2edd6842539cb74465f259cdcb4bbd957c6c4411
CCeph <yousefelsisyy@gmail.com>
2024-01-26 17:14:52 -0400

    Add state variables to game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,3 +1,9 @@
+import { useState } from "react";
+
 export default function Game() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [numberOfCats, setNumberOfCats] = useState(10);
+  const [cats, setCats] = useState([]);
   return <h1>The game is here</h1>;
 }
