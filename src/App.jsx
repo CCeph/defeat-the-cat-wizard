04fad3336281f8f2b53cbc94df0d84519f8f9433
CCeph <yousefelsisyy@gmail.com>
2024-01-26 17:12:39 -0400

    Change background position to ensure no overflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import CenteredPopup from "./components/CenteredPopup";
+import Game from "./components/Game";
 import "./App.css";
 
 function App() {
@@ -16,27 +17,28 @@ function App() {
           initialPopupVisibile ? "main-background blur" : "main-background"
         }
       ></div>
-      {
-        <CenteredPopup initialPopupVisibile={initialPopupVisibile}>
-          <h1>Defeat the Evil Cat Wizard</h1>
-          <p>
-            The cat wizard trapped all cats under his spell to build his own
-            empire. We must defeat him to return the cats to their rightful
-            owners. However, he is a cunning trickster. For each cat you save,
-            he conjurs an illusion of an exact look-alike. Attempting to save an
-            illusion will trap you under the wizard's spell, so move carefully!
-          </p>
-          <h2>Rules</h2>
-          <ul>
-            <li>Click on a cat's image to attempt to save it.</li>
-            <li>After clicking an image, all images are shuffled.</li>
-            <li>Don't click the same image twice.</li>
-            <li>Try to choose all images without duplication.</li>
-            <li>Defeat the cat wizard!</li>
-          </ul>
-          <button onClick={hideInitialPopup}>Venture Forth</button>
-        </CenteredPopup>
-      }
+
+      <Game></Game>
+
+      <CenteredPopup initialPopupVisibile={initialPopupVisibile}>
+        <h1>Defeat the Evil Cat Wizard</h1>
+        <p>
+          The cat wizard trapped all cats under his spell to build his own
+          empire. We must defeat him to return the cats to their rightful
+          owners. However, he is a cunning trickster. For each cat you save, he
+          conjurs an illusion of an exact look-alike. Attempting to save an
+          illusion will trap you under the wizard's spell, so move carefully!
+        </p>
+        <h2>Rules</h2>
+        <ul>
+          <li>Click on a cat's image to attempt to save it.</li>
+          <li>After clicking an image, all images are shuffled.</li>
+          <li>Don't click the same image twice.</li>
+          <li>Try to choose all images without duplication.</li>
+          <li>Defeat the cat wizard!</li>
+        </ul>
+        <button onClick={hideInitialPopup}>Venture Forth</button>
+      </CenteredPopup>
     </>
   );
 }
---
+++ src/components/Game.jsx
@@ -0,0 +1,3 @@
+export default function Game() {
+  return <h1>The game is here</h1>;
+}
