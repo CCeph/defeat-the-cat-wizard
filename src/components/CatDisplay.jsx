621d1c6970d15dcbdee1105570df05ea38cab3e5
cceph <96626515+CCeph@users.noreply.github.com>
2024-01-27 16:56:28 -0400

    Shuffle displayed photos

--- src/components/CatDisplay.jsx
+++ src/components/CatDisplay.jsx
@@ -1,5 +1,23 @@
 import { useState } from "react";
 
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
 export default function CatDisplay({setCurrentScore, setBestScore, numberOfCats, cats}) {
     const [chosenCats, setChosenCats] = useState([]);
     const catPhotos = cats.map((cat) => 
@@ -7,9 +25,11 @@ export default function CatDisplay({setCurrentScore, setBestScore, numberOfCats,
             <img src={cat.url} alt="cat-photo" />
         </div>
     )
+
+    const shuffledCatPhotos = shuffle(catPhotos);
     return (
         <div className="cat-display">
-            {catPhotos}
+            {shuffledCatPhotos}
         </div>
     )
 }
